f7d37126508e3f5b66e75b718f912f0ff19fbb86
brandondv93 <brandondv93@gmail.com>
2022-09-08 19:56:11 -0500

    CORS removal

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
-from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)
 bcrypt = Bcrypt(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
